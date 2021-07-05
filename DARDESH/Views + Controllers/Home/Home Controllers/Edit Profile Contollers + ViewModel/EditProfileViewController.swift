020f4e5716583a1b0cf0e7fe953b9a42301a1c97
Mohamed Ali <mo.ali12477@gmail.com>
2021-07-04 20:50:16 -0700

    init with Edit profile UI and add constrains and make new empty controller&ViewModel commit

---
+++ DARDESH/Views + Controllers/Home/Home Controllers/Edit Profile Contollers + ViewModel/EditProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  EditProfileViewController.swift
+//  DARDESH
+//
+//  Created by Mohamed Ali on 04/07/2021.
+//
+
+import UIKit
+
+class EditProfileViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ DARDESH/Views + Controllers/Home/Home Controllers/Edit Profile Contollers + ViewModel/EditProfileViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  EditProfileViewModel.swift
+//  DARDESH
+//
+//  Created by Mohamed Ali on 04/07/2021.
+//
+
+import Foundation
+import RxCocoa
+import RxSwift
+import ProgressHUD
+
+class EditProfileViewModel {
+    
+}
